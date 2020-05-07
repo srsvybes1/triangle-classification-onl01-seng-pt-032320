bfdc93c9d62d016e610615445968dee49a9a60a9
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-07 19:10:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,16 @@
 class Triangle
   attr_accessor :a, :b, :c
-
+@@all = []
   def initialize(a, b, c)
     @a = a
     @b = b
     @c = c
+    @@all << self 
   end
 
+def self.all
+  @@all 
+end 
   def kind
     validate_triangle
     if a == b && b == c
@@ -19,9 +23,9 @@ class Triangle
   end
 
   def validate_triangle
-    triangle = [ (a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each { |s| triangle << false if s <= 0}
-    raise TriangleError if triangle.include?(false)
+    self = [ (a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| self << false if s <= 0}
+    raise TriangleError if self.include?(false)
   end
 end
 
